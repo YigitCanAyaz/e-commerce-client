48a97047ad5ce4a874678b1a0c6d79c3edfa0e3a
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-23 19:32:51 +0300

    created admin/layout structure (module and components)

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,3 +1,4 @@
+import { LayoutModule } from './layout/layout.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,7 +7,11 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule
+  ],
+  exports: [
+    LayoutModule
   ]
 })
 export class AdminModule { }
